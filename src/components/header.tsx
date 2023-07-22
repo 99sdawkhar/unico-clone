5f126d2eb2e9b51f2a241e7fb1724d7fa80d872b
Shubham Dawkhar <99sdawkhar@gmail.com>
2023-07-22 19:17:09 +0530

    feat: basic header structure and styles

---
+++ src/components/header.tsx
@@ -0,0 +1,45 @@
+import { siteConfig } from "@/constants/siteConfig";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full bg-white">
+      <div className="wrapper m-auto w-[95%] max-w-[1100px]">
+        <div className="flex items-center justify-between py-1.5">
+          <h1>
+            <Link href={"/"}>
+              <Image src={siteConfig.logo.src} width={161} height={49} />
+            </Link>
+          </h1>
+          <nav>
+            <ul className="flex justify-between items-center">
+              {siteConfig.nav.map((menu) => (
+                <li key={menu.title} className="group">
+                  <Link
+                    href={menu.href}
+                    className={`block p-3.5 ${menu.secondary ? "bg-black text-white px-6 ml-2.5" : ""}`}
+                  >
+                    {menu.title}
+                  </Link>
+                  {Array.isArray(menu.dropdown) && menu.dropdown.length > 0 && (
+                    <ul className="hidden md:group-hover:block bg-black text-white flex flex-col absolute top-[90%]">
+                      {menu.dropdown.map((subMenu) => (
+                        <li key={subMenu.title}>
+                          <Link href={subMenu.href} className="block px-3.5 py-5">{subMenu.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/constants/siteConfig.ts
@@ -0,0 +1,80 @@
+export const siteConfig = {
+  logo: {
+    src: "https://uploads-ssl.webflow.com/6203b6d57823100847efd9b1/62a443e6434916672ae8ad5f_BLACK%20LOGO.svg",
+  },
+  nav: [
+    {
+      title: "Services",
+      href: "#FIXME",
+      dropdown: [
+        {
+          title: "Mobile App Development",
+          href: "#FIXME",
+        },
+        {
+          title: "Web Development",
+          href: "#FIXME",
+        },
+        {
+          title: "UI/UX Design",
+          href: "#FIXME",
+        },
+        {
+          title: "AI Development",
+          href: "#FIXME",
+        },
+        {
+          title: "Tech Consulting",
+          href: "#FIXME",
+        },
+      ],
+    },
+    {
+      title: "Hire Developers",
+      href: "#FIXME",
+      dropdown: [
+        {
+          title: "Mobile App Development",
+          href: "#FIXME",
+        },
+        {
+          title: "Web Development",
+          href: "#FIXME",
+        },
+        {
+          title: "UI/UX Design",
+          href: "#FIXME",
+        },
+        {
+          title: "AI Development",
+          href: "#FIXME",
+        },
+        {
+          title: "Tech Consulting",
+          href: "#FIXME",
+        },
+      ],
+    },
+    {
+      title: "Case Study",
+      href: "#FIXME",
+    },
+    {
+      title: "Pricing Plan",
+      href: "#FIXME",
+    },
+    {
+      title: "No Code Tools",
+      href: "#FIXME",
+    },
+    {
+      title: "About Us",
+      href: "#FIXME",
+    },
+    {
+      title: "Get in Touch",
+      href: "#FIXME",
+      secondary: true,
+    },
+  ],
+};
