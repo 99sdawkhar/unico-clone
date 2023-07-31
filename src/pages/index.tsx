8b492fe7ff9ce4fe002bebd28543dd625d17efd6
Shubham Dawkhar <99sdawkhar@gmail.com>
2023-08-01 00:44:26 +0530

    fix: added no index tag and basic seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,23 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import WhyChooseUs from '@/components/whyChooseUs'
 import Services from '@/components/services'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-      <Header />
-      <WhyChooseUs />
-      <Services />
-    </main>
+    <>
+      <Head>
+        <title>No Code & Code Development Company | Build Custom Web & Mobile Apps | Unico Connect</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      <main className={inter.className}>
+        <Header />
+        <WhyChooseUs />
+        <Services />
+      </main>
+    </>
   )
 }
